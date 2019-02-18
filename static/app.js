337530d54c0031f13b9fe024a8ebde489fcda250
Akshay <akshay.deep0@gmail.com>
2019-02-18 16:16:15 +0530

    order repos by stars

--- static/app.js
+++ static/app.js
@@ -1,16 +1,26 @@
+// sort items by star
+(function () {  
+  for (const key in __output) {
+    __output[key] = __output[key].sort((a, b) => {
+      return a.stars > b.stars ? -1 : 1;
+    });
+  }
+})();
+
+const buildDate = function(date){
+  const [d, m, y] = date.split("-")
+  return new Date(y, m - 1, d);
+};
+
 var app = new Vue({
   el: '#app',
   data: {
-    items: __output,
+    // items: __output,
   },
   computed:{
     itemsKeys: function(){
-      const date = function(date){
-        const [d, m, y] = date.split("-")
-        return new Date(y, m - 1, d);
-      };
       return Object.keys(__output).sort((a, b) => {
-        return date(a) > date(b) ? -1 : 1;
+        return buildDate(a) > buildDate(b) ? -1 : 1;
       });
     }, 
     items: function(){
